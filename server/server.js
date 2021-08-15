ab93cf26fe3cc82cfeb55fc5975c92060dea1f34
Alex Bright <abrighttt@gmail.com>
2021-08-15 18:48:09 -0400

    trying another heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
-const path = require("path");
 const db = require("./config/connection");
+const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,8 +22,10 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/public")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.use(routes); 
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/public/index.html"));
